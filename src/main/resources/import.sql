e1e82fb11abb905319e58cecbc089029df8ef061
SeongUk <pkseonguk@gmail.com>
2023-04-12 00:28:24 +0900

    Fix config

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,2 +1,1 @@
 INSERT INTO user (NAME, ROLL, SUB, CREATE_DATE, IS_ACTIVE) VALUES ('박성욱', 'ROLE_USER', '1234', now(), 1);
-https://no-banner.s3.ap-northeast-2.amazonaws.com/0c5907ff-78b1-4a51-82d8-f058cbe37e09-d.png
