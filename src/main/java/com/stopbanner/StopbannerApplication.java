ef83f6f199950bb5bef0f703b03be04da2b56f43
SeongUk <pkseonguk@gmail.com>
2023-04-12 00:22:25 +0900

    Fix cors

--- src/main/java/com/stopbanner/StopbannerApplication.java
+++ src/main/java/com/stopbanner/StopbannerApplication.java
@@ -1,7 +1,11 @@
 package com.stopbanner;
 
+import com.amazonaws.HttpMethod;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class StopbannerApplication {
@@ -10,4 +14,16 @@ public class StopbannerApplication {
 		SpringApplication.run(StopbannerApplication.class, args);
 	}
 
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/**")
+						.allowedOrigins("http://localhost:3000")
+						.allowedMethods(HttpMethod.GET.name())
+						.allowedMethods(HttpMethod.POST.name());
+			}
+		};
+	}
 }
