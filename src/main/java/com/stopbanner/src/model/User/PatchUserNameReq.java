e015d310d734c4ae12b68c81ea737816e214be3d
SeongUk <pkseonguk@gmail.com>
2023-04-30 12:40:20 +0900

    Add valid PATCH user/name

--- src/main/java/com/stopbanner/src/model/User/PatchUserNameReq.java
+++ src/main/java/com/stopbanner/src/model/User/PatchUserNameReq.java
@@ -3,6 +3,7 @@ package com.stopbanner.src.model.User;
 import lombok.*;
 
 import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.Size;
 
 @Getter
 @Setter
@@ -10,5 +11,6 @@ import javax.validation.constraints.NotEmpty;
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class PatchUserNameReq {
     @NotEmpty
+    @Size(min=1, max=10)
     private String name;
 }
